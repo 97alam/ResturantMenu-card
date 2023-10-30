023a4e1b5934577532035fed1dfd1d29d14be8aa
Naushad Alam <145100978+97alam@users.noreply.github.com>
2023-10-30 13:51:32 +0530

    Add files via upload

---
+++ menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggie.avif",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Maggi is made by a process similar to soy sauce, except with fermented wheat protein, so it has a deep, rich, and salty savoriness that cannot come from regular old salt and pepper. The best way to describe its actual flavor is “roasty.” It tastes like cooked...something. ",
+    },
+  
+    {
+      id: 2,
+      image: "images/aalupakoda.jpg",
+      name: "allu pakoida",
+      category: "evening",
+      price: "20₹",
+      description:
+        " it is a healthy source of fibre. Fibre is important for keeping you full, preventing constipation, and providing a lot of essential minerals, like calcium and iron. ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        " it's a good choice to use in place of wheat. It's also loaded with important vitamins, minerals, and antioxidants that can help your health in a number of ways.",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Also known as Rawalpindi chhole, this Punjabi delicacy is a surprise package of flavours. Whole spices and garam masala along with tea leaves give this preparation its unique taste and colour. The chickpeas are prepared dry with a light hint of gravy and are best mixed with hot steamed rice.",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Pizza is an Italian food that was created in Italy. It is made with different toppings. Some of the most common toppings are cheese, sausages, pepperoni, vegetables, tomatoes, spices and herbs and basil. These toppings are added over a piece of bread covered with sauce. ",
+    },
+    {
+      id: 6,
+      image: "../images/NonVeg.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "any food which contains whole or part of any animal including birds, marine animals, eggs, or products of any animal origin as an ingredient, excluding milk or milk products.",
+    },
+    {
+      id: 7,
+      image: "../images/Sweets.avif",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Sweet is a type of food that is eaten after lunch or dinner, and sometimes after a light meal or snack. It is usually a sweet food, like ice cream, cookies, and cakes.",
+    },
+    {
+      id: 8,
+      image: "../images/Rajma Rice.webp",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "This simple Indian dish consists of red beans (rajma) and cooked rice (chawal). Rajma is traditionally made with onions and tomato purée which acts as a gravy for the red beans. This dish makes for a complete vegetarian meal that is suitable for lunch or dinner.",
+    },
+    {
+      id: 9,
+      image: "../images/samaso.jpg",
+      name: "samosa",
+      category: "evening",
+      price: "10₹",
+      description:
+        "A samosa is a fried South Asian pastry with a savoury filling, including ingredients such as spiced potatoes, onions, peas, meat or fish. It may take different forms, including triangular, cone, or half-moon shapes, depending on the region.",
+    },
+  ];
+  
+  export default Menu;
